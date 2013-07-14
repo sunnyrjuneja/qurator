19702dfc47f5452ae3d1dba57b1bbe8e2905d165
Sunny Juneja <me@sunnyjuneja.com>
2013-07-14 12:48:55 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qurator::Application.config.secret_key_base = 'd8f49a04036113d0ee6f05774e69e77cab3edad6d31c14969b1594f9be1edaf117737c0e5a8a6afabf0b0e05493af6fc59945c7d9a7c0616894e8479d4eaa9bf'
